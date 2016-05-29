55a1f39263525780224417aedba017321da4a5ca
abhinavchawla13 <kii@Kiis-MacBook-Pro.local>
2016-05-29 14:53:24 -0400

    Salt Selection Complete

--- www/js/services/medicine.js
+++ www/js/services/medicine.js
@@ -14,6 +14,8 @@
     this.selectedMedicine;
     this.currentMedicineSalt;
     this.suggestions;
+    this.probableAllergicSalts = [];
+    this.definiteAllergicSalts = [];
     return service;
 
     //key: 19672e3aa4204c3de62095597d5947
@@ -22,8 +24,7 @@
     }
 
     function getSaltFromMedicine(medicine){
-      console.log('1:::::' + '/apiSaltFromMedicine' + '?id=' + medicine +'&key=' + '19672e3aa4204c3de62095597d5947');
-      return $http.get('/apiSaltFromMedicine' + '?id=' + medicine +'&key=' + '19672e3aa4204c3de62095597d5947');
+      return $http.get('/apiSaltFromMedicine?id=' + encodeURIComponent(medicine) +'&key=' + '19672e3aa4204c3de62095597d5947');
     }
 
   }
--- www/templates/saltSelection/salt.selection.controller.js
+++ www/templates/saltSelection/salt.selection.controller.js
@@ -9,6 +9,9 @@
     function saltSelectionController($timeout, $state, UserService, MedicineService, $ionicHistory, $ionicLoading) {
         var vm = this;
         vm.goBack = goBack;
+        vm.notSure = notSure;
+        vm.finalSure = finalSure;
+        vm.checkSingleItemSelected = checkSingleItemSelected;
 
         load();
         return vm;
@@ -24,10 +27,8 @@
               .getSaltFromMedicine(MedicineService.selectedMedicine)
               .then(function(response) {
                   vm.salts = response.data.response.constituents;
-                  console.log('old', vm.salts);
                   MedicineService.currentMedicineSalt = makeList(vm.salts);
                   vm.salts = MedicineService.currentMedicineSalt;
-                  console.log(vm.salts);
                   $ionicLoading.hide();
               }, function(error) {
                   console.log(error);
@@ -37,9 +38,47 @@
         function makeList(saltsData){
           var saltList = [];
           angular.forEach(saltsData, function(value){
-              saltList.push(value.name);
+              saltList.push({name: value.name, check: false});
           });
+          saltList = processSaltData(saltList);
           return saltList;
+        }
+
+        function processSaltData(saltList){
+          if (saltList.length == 1){
+            saltList[0].check = true;
+          }
+          return saltList;
+        }
+
+        function notSure(){
+          angular.forEach(vm.salts, function(value){
+            if (MedicineService.probableAllergicSalts.indexOf(value.name) == -1){
+              MedicineService.probableAllergicSalts.push(value.name);
+            }
+          });
+          console.log(MedicineService.probableAllergicSalts);
+        }
+
+        function checkSingleItemSelected(){
+          var check = false;
+          angular.forEach(vm.salts, function(value){
+            if (value.check){
+              check = true;
+            }
+          });
+          return check;
+        }
+
+        function finalSure(){
+          angular.forEach(vm.salts, function(value){
+            if (value.check){
+              if (MedicineService.definiteAllergicSalts.indexOf(value.name) == -1){
+                MedicineService.definiteAllergicSalts.push(value.name);
+              }
+            }
+          });
+          console.log(MedicineService.definiteAllergicSalts);
         }
 
     }
